229e6f8d6cad26f1428938359242ff67b5f54b2e
AxAd <asadhusn007@gmail.com>
2023-10-16 23:08:18 +0500

    inches to cm func

--- Inches to Centimeters.c
+++ Inches to Centimeters.c
@@ -1,14 +1,19 @@
 // convert height inches to centimeters using 2.54 * height
 #include<stdio.h>
+
+// converter
+float inches_to_cm(float inches){
+	return inches * 2.54;
+}
+
+// driver
 int main()
 {
-	short inches;
-	float centimeters;
-	printf("Enter height in inches : ");
-	scanf("%d",&inches);
-	
-	centimeters=2.54*inches;
-	
-	printf("\n---output---\n");
-	printf("Centimeters = %f",centimeters);
+	puts("--- inches to cm ---");
+	float inches;
+	printf("Enter inches: ");
+	scanf("%f", &inches);
+	float cm = inches_to_cm(inches);
+	printf("Centimeters = %f", cm);
 }
+
